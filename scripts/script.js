37fa1b90eeaf6b61ae4e05284cf18bdcdbcdf115
mariognDev <mariogn1987@gmail.com>
2025-06-16 11:56:03 +0200

    navbar and footer

--- scripts/script.js
+++ scripts/script.js
@@ -65,17 +65,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* funtion to grow textarea of contact if its necesary*/
 
-function adjustHeight(event) {
-	const textarea = event.target;
-	if (!textarea.dataset.minHeight) {
-		textarea.dataset.minHeight = textarea.offsetHeight;
-	}
-	const minHeight = parseInt(textarea.dataset.minHeight, 10);
-	if (textarea.scrollHeight > textarea.offsetHeight) {
-		textarea.style.height = 'auto'; 
-		textarea.style.height = `${Math.max(textarea.scrollHeight, minHeight)}px`;
-	}
-}
-const textarea = document.getElementById('textareaMessage');
-textarea.addEventListener('input', adjustHeight);
+// function adjustHeight(event) {
+// 	const textarea = event.target;
+// 	if (!textarea.dataset.minHeight) {
+// 		textarea.dataset.minHeight = textarea.offsetHeight;
+// 	}
+// 	const minHeight = parseInt(textarea.dataset.minHeight, 10);
+// 	if (textarea.scrollHeight > textarea.offsetHeight) {
+// 		textarea.style.height = 'auto'; 
+// 		textarea.style.height = `${Math.max(textarea.scrollHeight, minHeight)}px`;
+// 	}
+// }
+// const textarea = document.getElementById('textareaMessage');
+// textarea.addEventListener('input', adjustHeight);
 
